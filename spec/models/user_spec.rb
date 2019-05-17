dde9fe93bda2918dae9db7922ec4ddc86bf475bd
wipegup <wipegup@gmail.com>
2019-05-17 15:07:26 -0600

    Passing user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:password) }
   end
 end
