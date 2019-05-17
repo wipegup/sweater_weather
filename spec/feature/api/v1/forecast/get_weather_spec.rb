32d1d706778d7348b3bb8e40081266ebbf616759
wipegup <wipegup@gmail.com>
2019-05-17 14:33:13 -0600

    Mock response from darksky api

--- spec/feature/api/v1/forecast/get_weather_spec.rb
+++ spec/feature/api/v1/forecast/get_weather_spec.rb
@@ -1,5 +1,11 @@
+require 'rails_helper'
 describe 'call to forecast with location' do
   scenario 'returns all forecast data' do
+    url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API']}/37.8267,-122.4233"
+    body =  File.open('./api_responses/dark_sky.json')
+    stub_request(:get, url).to_return(body: body)
 
+    f = Faraday.get(url)
+    
   end
 end
