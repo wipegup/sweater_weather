b835f182b3bd907c2fd723a3e90135a5b872f485
wipegup <wipegup@gmail.com>
2019-05-17 15:17:38 -0600

    Added secure api_key to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, on: :create
 
+  has_secure_token :api_key
   has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_210311) do
+ActiveRecord::Schema.define(version: 2019_05_17_211606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2019_05_17_210311) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.string "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
+    t.index ["api_key"], name: "index_users_on_api_key", unique: true
   end
 
 end
