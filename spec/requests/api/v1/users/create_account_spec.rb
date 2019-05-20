23f971c5399fc6027fd0fa66f18ca8c8d8a4e650
wipegup <wipegup@gmail.com>
2019-05-19 22:53:24 -0600

    Update test for creating user

--- spec/requests/api/v1/users/create_account_spec.rb
+++ spec/requests/api/v1/users/create_account_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe 'Users API' do
   it 'can create an account' do
-
-    post '/api/v1/users'
+    user = attributes_for(:user)
+    post '/api/v1/users', headers: {"ACCEPT" => "application/json",
+                                   'email'=> user[:email],
+                                   'password'=> user[:password_digest],
+                                   'password_confirmation'=> user[:password_digest]}
 
     expect(response).to be_successful
+    created_user = User.last
+
+    expect(created_user.email).to eq(user[:email])
+    expect(created_user.api_key).not_to eq(nil)
   end
 end
