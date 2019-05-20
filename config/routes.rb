781206013d1c07641ca6226bc7da15249ea633a1
wipegup <wipegup@gmail.com>
2019-05-19 22:17:20 -0600

    Simple session test passing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'forecast', to: 'forecast#show'
+      post 'sessions', to: 'sessions#create'
+      
     end
 
   end
