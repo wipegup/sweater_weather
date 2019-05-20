e622d70d9b906af887141bb349c9e622ab579c0f
wipegup <wipegup@gmail.com>
2019-05-19 22:53:48 -0600

    User creation successful

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,11 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
+    email = request.headers['email']
+    password = request.headers['password']
+    password_confirmation = request.headers['password_confirmation']
 
+    user = User.new(email: email, password: password, password_confirmation: password_confirmation)
+    user.save
   end
 end
