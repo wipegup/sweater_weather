1eb277ebef6558be4b858e253e6c994b5a91132a
wipegup <wipegup@gmail.com>
2019-05-19 22:19:17 -0600

    Routing test for backgrounds passing

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::BackgroundsController < ApplicationController
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'forecast', to: 'forecast#show'
       post 'sessions', to: 'sessions#create'
-      
+      get 'backgrounds', to: 'backgrounds#show'
     end
 
   end
