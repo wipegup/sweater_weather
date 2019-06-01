2bd3596738914bc2d9cacf2c6fd38b990c5766d8
wipegup <wipegup@gmail.com>
2019-06-01 14:16:17 -0600

    Add stubs for forecast test -- commented out

--- spec/requests/api/v1/forecast/get_weather_spec.rb
+++ spec/requests/api/v1/forecast/get_weather_spec.rb
@@ -10,12 +10,5 @@ describe 'Forecast API' do
 
     expect(response).to be_successful
 
-    expect(response['results'][0]['location']['lat']).to eq(latitude)
-    expect(response['results'][0]['location']['lng']).to eq(longitude)
-    expect(response['results'][0]['formatted_address']).to eq("Denver, CO, USA")
-    url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API']}/39.7392358,-104.990251"
-    body =  File.open('./api_responses/dark_sky_denver.json')
-    stub_request(:get, url).to_return(body: body)
-
   end
 end
--- spec/requests/api/v1/forecast/return_forecast_for_city_spec.rb
+++ spec/requests/api/v1/forecast/return_forecast_for_city_spec.rb
@@ -4,6 +4,14 @@ describe 'request using city' do
   it 'returns forecast for that city' do
     get '/api/v1/forecast?location=denver,co'
 
+    # ds_url = "https://api.darksky.net/forecast/#{ENV['DARK_SKY_API']}/39.7392358,-104.990251"
+    # ds_body =  File.open('./api_responses/dark_sky_denver.json')
+    # stub_request(:get, ds_url).to_return(body: ds_body)
+    #
+    # geo_url = "https://maps.googleapis.com/maps/api/geocode/json?address=denver,co&key=#{ENV['GOOGLE_API']}"
+    # geo_body =  File.open('./api_responses/geocode_denver.json')
+    # stub_request(:get, geo_url).to_return(body: geo_body)
+
     expect(response).to be_successful
 
     forecast = JSON.parse(response.body, symbolize_names: true)
