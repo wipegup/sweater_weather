82160eb7afc740f2fe3b0821c3fbefd4e956d54e
wipegup <wipegup@gmail.com>
2019-06-01 13:20:41 -0600

    remove mocking

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
-require 'vcr'
+# require 'vcr'
 # require 'webmock/rspec'
 
 # VCR.configure do |config|
