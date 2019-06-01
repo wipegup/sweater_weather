b4af137f789f7c3ea923445db20edf421c70432b
wipegup <wipegup@gmail.com>
2019-06-01 14:32:10 -0600

    Test for returning apikey with login

--- spec/requests/api/v1/users/login_spec.rb
+++ spec/requests/api/v1/users/login_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 describe 'Sessions API' do
   it 'logs in user' do
-
-    post '/api/v1/sessions'
+    user = create(:user, password: "password")
+    post '/api/v1/sessions', headers: {"email": user.email,
+                                       "password": "password"}
 
     expect(response).to be_successful
+
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    expect(json_response[:api_key]).to eq(created_user.api_key)
   end
 end
