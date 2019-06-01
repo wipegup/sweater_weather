a31a729955cef93c42a2a0d2aa2ae051c97eb7ba
wipegup <wipegup@gmail.com>
2019-06-01 15:12:38 -0600

    increase testing

--- spec/requests/api/v1/users/login_spec.rb
+++ spec/requests/api/v1/users/login_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe 'Sessions API' do
   it 'logs in user' do
     user = create(:user, password: "password")
+
+    post '/api/v1/sessions', headers: {"email": user.email,
+                                       "password": "not_correct"}
+
+    expect(response).to have_http_status(401)
+    
     post '/api/v1/sessions', headers: {"email": user.email,
                                        "password": "password"}
 
