25801ce068a3bc1c2bfdb933e3c8b9a2085f5c93
wipegup <wipegup@gmail.com>
2019-06-01 13:20:54 -0600

    Add expectations about format of return

--- spec/requests/api/v1/forecast/return_forecast_for_city_spec.rb
+++ spec/requests/api/v1/forecast/return_forecast_for_city_spec.rb
@@ -9,6 +9,13 @@ describe 'request using city' do
     latitude = 39.7392358
     longitude = -104.990251
     expect(response['latitude']).to eq(latitude)
-    expect(response['longitude']).to eq(longitued)
+    expect(response['longitude']).to eq(longitude)
+    keys =  %w(currently minutely hourly daily)
+
+    keys.each do |key|
+      expect(response.keys).to include(key)
+    end
+
+
   end
 end
