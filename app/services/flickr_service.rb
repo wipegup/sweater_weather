401d83f31fe31431f8eb5927c608f156c970c61a
wipegup <wipegup@gmail.com>
2019-06-03 11:23:24 -0600

    Finding photos by lat long

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -2,7 +2,10 @@ class FlickrService
   include ApiService
 
   def landscape(location)
-    conn.get('', {tags: location+",landscape"} )
+    lat_long = GeocodeService.new.lat_long(location).split(",")
+    lat_long = {lat: lat_long[0].to_s, lon: lat_long[1].to_s}
+
+    conn.get('', {accuracy: "10", **lat_long} )
   end
 
   private
@@ -16,5 +19,7 @@ class FlickrService
       f.params['nojsoncallback'] = '1'
       f.params['api_key'] = '430449191412d4e1430ea71139efdd23'
     end
+
   end
+
 end
