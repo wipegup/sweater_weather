9b70afb47d5642596ff4ccedf5f424421ddfa95d
wipegup <wipegup@gmail.com>
2019-06-04 09:40:32 -0600

    Pull out format lat long to general method

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,14 +1,29 @@
 class GeocodeService
   include ApiService::ConvenienceMethods
-  def lat_long(location)
+  def lat_long(location, as = String)
      response = conn.get('json', 'address': location)
      json = parse(response.body)
      hash = json[:results][0][:geometry][:location]
-     [hash[:lat].to_s, hash[:lng].to_s].join(",")
+     format_lat_long(hash, as)
    end
 
  private
 
+ def format_lat_long(hash, as)
+   lat = hash[:lat].to_s
+   lng = hash[:lng].to_s
+   if as == String
+     return [lat,lng].join(",")
+   elsif as == Array
+     return [lat,lng]
+   elsif as.class == Array
+     return {as[0]=>lat, as[1]=>lng}
+   else
+     puts "Incorrect format"
+   end
+
+
+ end
  def conn
    Faraday.new("https://maps.googleapis.com/maps/api/geocode/") do |f|
      f.adapter Faraday.default_adapter
